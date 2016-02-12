c48f8e0c33d9639328c4ca0b6e950e132bdb8cf6
Mike Hawk <jonathanimson@gmail.com>
2016-02-12 10:42:02 -0800

    took out the error that came with homeservice js file

--- public/javascript/services/HomeService.js
+++ public/javascript/services/HomeService.js
@@ -23,26 +23,3 @@ var app;
         angular.module('app').service('HomeService', HomeService);
     })(Services = app.Services || (app.Services = {}));
 })(app || (app = {}));
-
-"use strict";
-var app;
-(function (app) {
-    var Services;
-    (function (Services) {
-        var HomeService = (function () {
-            function HomeService($resource) {
-                this.$resource = $resource;
-                this.BeerResource = $resource('/api/v1/beer/:id');
-            }
-            HomeService.prototype.getAll = function () {
-                return this.BeerResource.query();
-            };
-            HomeService.prototype.saveBeer = function (beer) {
-                return this.BeerResource.save(beer).$promise;
-            };
-            return HomeService;
-        }());
-        Services.HomeService = HomeService;
-        angular.module('app').service('HomeService', HomeService);
-    })(Services = app.Services || (app.Services = {}));
-})(app || (app = {}));
