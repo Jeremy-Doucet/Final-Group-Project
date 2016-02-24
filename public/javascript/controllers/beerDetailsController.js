f7e8a437e632f5cc0e65887849bc96acffce1767
Mike Hawk <jonathanimson@gmail.com>
2016-02-23 19:11:06 -0800

    changed like function

--- public/javascript/controllers/beerDetailsController.js
+++ public/javascript/controllers/beerDetailsController.js
@@ -4,19 +4,20 @@ var app;
     var Controllers;
     (function (Controllers) {
         var beerDetailsController = (function () {
-            function beerDetailsController(commentService, $location, homeService, $routeParams) {
+            function beerDetailsController(commentService, $location, homeService, $routeParams, userService) {
                 var _this = this;
                 this.commentService = commentService;
                 this.$location = $location;
                 this.homeService = homeService;
                 this.$routeParams = $routeParams;
+                this.userService = userService;
                 this.comments = [];
                 this.showModal = false;
                 this.heart = false;
                 homeService.getBeer($routeParams['id']).then(function (res) {
                     _this.beer = res;
                 });
-                this.beerExists = commentService.getAllLikes();
+                this.status = userService.status;
             }
             beerDetailsController.prototype.toggleLike = function () {
                 this.heart = !this.heart;
@@ -50,15 +51,18 @@ var app;
                 });
             };
             beerDetailsController.prototype.likeBeer = function () {
+                var _this = this;
                 var likedBeer = {
                     beer: this.beer._id,
                 };
                 this.commentService.saveLikedBeer(likedBeer).then(function (res) {
+                    _this.beer.likedByUsers.push(_this.status._id);
                 });
             };
             beerDetailsController.prototype.unlikeBeer = function (beer) {
+                var _this = this;
                 this.commentService.deleteLikedBeer(this.beer).then(function (res) {
-                    console.log('Beer was unliked');
+                    _this.beer.likedByUsers.splice(_this.beer.likedByUsers.indexOf(_this.status._id), 1);
                 });
             };
             beerDetailsController.prototype.rateBeer = function (rating) {
